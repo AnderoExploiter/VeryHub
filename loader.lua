70008e5cfc2e81e81c38520d961bd3e48db25fb7
AnderoExploiter <anderoexploits@gmail.com>
2024-08-29 19:04:39 +0700

    main

---
+++ loader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 6403373529 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/AnderoExploiter/VeryHub/main/games/Slap_Battles.lua'))()
+    else
+        error("Game Unsuported")
+    end
