c8689e9ba56eed1846b1c53dce0ccf7dc6db8f9d
AnderoExploiter <anderoexploits@gmail.com>
2024-08-29 19:08:51 +0700

    wqe

--- loader.lua
+++ loader.lua
@@ -1,4 +1,13 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
 if game.PlaceId == 6403373529 then
+    OrionLib:MakeNotification({
+        Name = "Info",
+        Content = "Game Supported (Slap Battles) ID - 6403373529",
+        Image = "rbxassetid://6774485594",
+        Time = 7
+    })
+
     loadstring(game:HttpGet('https://raw.githubusercontent.com/AnderoExploiter/VeryHub/main/games/Slap_Battles.lua'))()
     else
         error("Game Unsuported")
